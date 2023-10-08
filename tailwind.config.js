d03c336c3dccae2effa3162d7e55fcfb314c8c3d
KirschX <mk44879@gmail.com>
2023-10-08 21:08:34 +0900

    chore(setup): svgr, tailwind and other libraries installed and configured

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,20 @@
 /** @type {import('tailwindcss').Config} */
+const BASE = 16; // your base size
+
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        WineBerry: "#A30D27",
+        LightningYellow: "#FEBB15",
+        gray: "#C4C4C4",
+        White60: "#F2F2F2",
+        Sub_Blues: "#82B0F4",
+        Black_80: "#6D6D6D",
+        Blue_Main: "#3478F6",
+      },
+    },
   },
   plugins: [],
 };
