e4bd05e426bdd6cf8be72027242a2ff4c8037dca
KirschX <mk44879@gmail.com>
2023-10-08 21:10:29 +0900

    chore(setup): added constants

---
+++ src/constants/labels.ts
@@ -0,0 +1,19 @@
+const FILTER_LABELS = {
+  headline: {
+    label: "헤드라인",
+    placeholder: "검색하실 헤드라인을 입력하세요",
+  },
+  beginDate: {
+    label: "시작 날짜",
+    placeholder: "날짜를 입력해주세요",
+  },
+  endDate: {
+    label: "종료 날짜",
+    placeholder: "날짜를 입력해주세요",
+  },
+  glocations: {
+    label: "국가",
+  },
+};
+
+export default FILTER_LABELS;
